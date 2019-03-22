9d1127e3e73bc8657aa02e75c6911b031cd73444
zero <zero@sqrls.de>
2019-03-22 17:34:09 +0100

    export the builde

--- lib/tetris_blocs.dart
+++ lib/tetris_blocs.dart
@@ -4,3 +4,4 @@ library tetris_blocs;
 export 'package:tetris_blocs/core/tetris_event.dart';
 export 'package:tetris_blocs/core/tetris_bloc.dart';
 export 'package:tetris_blocs/core/tetris_provider.dart';
+export 'package:tetris_blocs/core/tetris_builder.dart';
